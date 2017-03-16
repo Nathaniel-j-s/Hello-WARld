6da92ddafb21c3ba2c4a22656c6827df21ee6f1b
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-03-16 15:53:54 -0600

    basic functionality done

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,20 @@
 angular.module('myApp').controller('mainCtrl', function($scope, mainSrvc) {
-  $scope.test = "Text conversion!";
+
+  $scope.convertSentence = function(kanjiSentence) {
+    mainSrvc.convertSentence(kanjiSentence).then(function(response) {
+      $scope.hira = response.data.converted.split(' ').join('');
+
+      $scope.breakUp = function(hira) {
+        mainSrvc.breakUp(hira).then(function(response) {
+          $scope.almost = response.data.word_list;
+          $scope.final = $scope.almost[0];
+        });
+      };
+      $scope.breakUp($scope.hira);
+    });
+  };
+
+
+console.log($scope.final);
+
 });
--- js/service.js
+++ js/service.js
@@ -1,3 +1,33 @@
 angular.module('myApp').service('mainSrvc', function($http) {
-  
+
+  this.convertSentence = function(kanjiSentence) {
+    return $http( {
+      method: 'POST',
+      url: 'https://labs.goo.ne.jp/api/hiragana',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: $.param({
+        "app_id": 'ca81727daf11523cc1c1a3e235f3c1767c4a1c1b68aec6640347ebf13d59669c',
+        "sentence": kanjiSentence,
+        "output_type":'hiragana'
+      })
+    });
+  };
+
+  this.breakUp = function(hira) {
+    return $http( {
+      method: 'POST',
+      url: 'https://labs.goo.ne.jp/api/morph',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: $.param({
+        "app_id": 'ca81727daf11523cc1c1a3e235f3c1767c4a1c1b68aec6640347ebf13d59669c',
+        "sentence": hira,
+        "info_filter": "form"
+      })
+    });
+  };
+
 });
